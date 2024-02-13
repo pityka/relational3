ee18ee53dab32538e15016c870f8d4e82c875c25
Istvan Bartha <106101+pityka@users.noreply.github.com>
2024-02-13 20:59:49 +0100

    Fix csv callback

--- core/src/main/scala/ra3/csv.scala
+++ core/src/main/scala/ra3/csv.scala
@@ -155,7 +155,7 @@ object csv {
       extends Callback {
 
     val locs = columnTypes.map(_._1)
-    private val locsIdx = org.saddle.Index(locs)
+    private val locsIdx : org.saddle.Index[Int] = org.saddle.Index(locs)
 
     val headerFields = scala.collection.mutable.ArrayBuffer[String]()
     val allHeaderFields = scala.collection.mutable.ArrayBuffer[String]()
@@ -296,9 +296,9 @@ object csv {
             if (loc >= headerAllFields) {
               error = true
               errorString =
-                s"Too long line ${line + 1} (1-based). Expected $headerAllFields fields, got ${loc + 1}."
+                s"Too long line ${line + 1} (1-based). Expected $headerAllFields fields, got ${loc + 1}.  "
             } else {
-              add(s, fromi, ptoi, loc)
+              add(s, fromi, ptoi, locsIdx.getFirst(loc))
             }
           }
         }
