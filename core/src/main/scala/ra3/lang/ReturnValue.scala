ffc21916a3ee74cf43285648f26698bef06b7efd
Istvan Bartha <106101+pityka@users.noreply.github.com>
2024-09-26 22:17:50 +0200

    seal and final columnspec

--- core/src/main/scala/ra3/lang/ReturnValue.scala
+++ core/src/main/scala/ra3/lang/ReturnValue.scala
@@ -2,7 +2,7 @@ package ra3.lang
 
 import ra3.*
 
-private[ra3] trait ColumnSpec[+T]
+private[ra3] sealed trait ColumnSpec[+T]
 private[ra3] sealed trait ColumnSpecWithValue[T] extends ColumnSpec[T] {
   def value: T
 }
@@ -12,36 +12,36 @@ private[ra3] sealed trait NamedColumnSpec[T] extends ColumnSpecWithValue[T] {
 private[ra3] sealed trait UnnamedColumnSpec[T] extends ColumnSpecWithValue[T] {
   def withName(s: String): NamedColumnSpec[T]
 }
-private[ra3] case class NamedColumnChunk(
+private[ra3] final case class NamedColumnChunk(
     value: Either[TaggedBuffer, TaggedSegments],
     name: String
 ) extends NamedColumnSpec[Either[TaggedBuffer, TaggedSegments]]
-private[ra3] case class NamedConstantI32(value: Int, name: String)
+private[ra3] final case class NamedConstantI32(value: Int, name: String)
     extends NamedColumnSpec[Int]
-private[ra3] case class NamedConstantI64(value: Long, name: String)
+private[ra3] final  case class NamedConstantI64(value: Long, name: String)
     extends NamedColumnSpec[Long]
-private[ra3] case class NamedConstantString(value: String, name: String)
+private[ra3] final case class NamedConstantString(value: String, name: String)
     extends NamedColumnSpec[String]
-private[ra3] case class NamedConstantF64(value: Double, name: String)
+private[ra3] final  case class NamedConstantF64(value: Double, name: String)
     extends NamedColumnSpec[Double]
-private[ra3] case class UnnamedColumnChunk(value: Either[TaggedBuffer, TaggedSegments])
+private[ra3] final case class UnnamedColumnChunk(value: Either[TaggedBuffer, TaggedSegments])
     extends UnnamedColumnSpec[Either[TaggedBuffer, TaggedSegments]] {
   def withName(s: String): NamedColumnChunk = NamedColumnChunk(value, s)
 }
-private[ra3] case class UnnamedConstantI32(value: Int)
+private[ra3] final case class UnnamedConstantI32(value: Int)
     extends UnnamedColumnSpec[Int] {
   def withName(s: String): NamedColumnSpec[Int] = NamedConstantI32(value, s)
 }
-private[ra3] case class UnnamedConstantI64(value: Long)
+private[ra3] final case class UnnamedConstantI64(value: Long)
     extends UnnamedColumnSpec[Long] {
   def withName(s: String): NamedColumnSpec[Long] = NamedConstantI64(value, s)
 }
-private[ra3] case class UnnamedConstantString(value: String)
+private[ra3] final  case class UnnamedConstantString(value: String)
     extends UnnamedColumnSpec[String] {
   def withName(s: String): NamedColumnSpec[String] =
     NamedConstantString(value, s)
 }
-private[ra3] case class UnnamedConstantF64(value: Double)
+private[ra3] final case class UnnamedConstantF64(value: Double)
     extends UnnamedColumnSpec[Double] {
   def withName(s: String): NamedColumnSpec[Double] = NamedConstantF64(value, s)
 }
